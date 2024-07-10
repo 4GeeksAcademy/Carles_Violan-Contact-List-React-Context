eb3c3dcba18ec569e2cda9b0c5184990d74359e5
AJPadillo <98108563+AJPadillo@users.noreply.github.com>
2024-07-10 14:32:14 +0000

    prueba1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,81 +1,51 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			//Simulacion del fetch GET contacts
+			slug: "AJPadillo",
+			agenda: [],
 			contacts: [
 				{
-					"name": "Antonio",
-					"phone": "555555",
-					"email": "string",
-					"address": "string",
-					"id": 3
-				},
-				{
-					"name": "Antonio 2",
-					"phone": "33333333",
-					"email": "string",
-					"address": "string",
-					"id": 68
-				},
-				{
-					"name": "Antonio 3",
-					"phone": "22222222",
-					"email": "string",
-					"address": "string",
-					"id": 11
+					"name": "",
+					"phone": "",
+					"email": "",
+					"address": "",
+					"id": 0
 				}
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getAgenda: async () => {
+				try {
+					const response = await fetch("https://playground.4geeks.com/contact/agendas/AJPadillo")
+					if (response.status != 200) {
+						console.log(response.status)
+					}
+					const data = await response.json()
+					setStore({ agenda: data});
+				} catch (error) {
+					console.log(error);
+				}
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			newContact: async (newContactData) => {
+				try {
+					const API_URL = "https://playground.4geeks.com/apis/fake/contact/";
+					const requestConfig = {
+						method: "POST",
+						headers: {
+							"Content-type": "application/json"
+						},
+						body: JSON.stringify(newContactData)
+					}
+					const response = await fetch(API_URL, requestConfig); 
+						if (response.status != 201) {
+						console.log("Error requesting. Code: ", response.status)
+					}
+					const body = await response.json()
+					setStore({ newContact: body});
+				} catch(error) {
+					console.log(error)
+				}
 			},
-			resetColor: (index) => {
-				getActions().changeColor(index, "white");
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			//Boton de Log de prueba
-			getContactNames: () => {
-				const store = getStore();
-				const contactNames = store.contacts.map(contact => contact.name);
-				return contactNames;
-			},
-			logNames: () => {
-				const log = getActions().getContactNames();
-				console.log(log);
-			}
-			//Fin de boton de prueba
 		}
 	};
 };
